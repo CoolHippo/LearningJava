69dfc9506c58d3bcc009a43196483ab918c3ed1a
Saton <coolhippo@yandex.ru>
2016-05-01 23:32:06 +0500

    Simple version Two print command

---
+++ src/SimpleClass/MyClass.java
@@ -0,0 +1,9 @@
+package SimpleClass;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("Hello it is Java");
+
+    }
+}
