d0bc7075c39ec81c3b83de190e80f6ca7b53c26c
Saton <coolhippo@yandex.ru>
2016-05-05 22:49:54 +0500

    added to fill the array with random numbers

--- src/SimpleClass/MyClass.java
+++ src/SimpleClass/MyClass.java
@@ -1,9 +1,31 @@
 package SimpleClass;
 
+import java.util.Scanner;
+
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
-        System.out.println("Hello it is Java");
+        String a;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number of cell array");
+        int numArray = scan.nextInt();
+        int[] array = new int [numArray];
+        for(int i = 0; i < array.length; i++){
+            array[i] = (int)(Math.random()*numArray);
+        }
+        do{
+
+            System.out.println("Continue - enter \"no\"");
+            a = scan.next();
+            if(a.equals("no")){
+                System.out.println("Enter the array index from 0 - "+(numArray - 1));
+                int b = scan.nextInt();
+                try {
+                    System.out.println(array[b]);
+                }catch (ArrayIndexOutOfBoundsException e){
+                    System.out.println("Enter the correct index array");
+                }
+            }
+        }while(a.equals("no"));
 
     }
 }
